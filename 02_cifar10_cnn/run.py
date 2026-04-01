978620c450d69148a83128c10e23bf877b15ebf4
AJY <skyscraper2936@gmail.com>
2026-04-01 17:14:33 +0900

    chore: relocate run.py and main.ipynb

--- 02_cifar10_cnn/run.py
+++ 02_cifar10_cnn/run.py
@@ -5,7 +5,7 @@ import torchvision.transforms as transforms
 import torch.optim as optim
 import argparse
 import numpy as np
-
+from sklearn.model_selection import train_test_split
 from model import CNN as BaselineCNN
 from mini_tasks.task1 import CNN as Task1CNN
 from train import train, evaluate
@@ -15,6 +15,29 @@ def get_model(name):
         return BaselineCNN()
     elif name == "task1":
         return Task1CNN()
+
+def split_train_val_data(full_train_datasets, train_ratio = 0.9, seed = 42):
+    train_indices = []
+    val_indices = []
+    targets = np.array(full_train_datasets.targets)
+    class_numbers = np.unique(targets)
+    np.random.seed(seed)
+
+    for i in class_numbers:
+      class_indices = np.where((i == targets))[0]
+      np.random.shuffle(class_indices)
+
+      train_size = int(train_ratio * len(class_indices))
+      train_indices.extend(class_indices[:train_size])
+      val_indices.extend(class_indices[train_size:])
+    
+    np.random.shuffle(train_indices)
+    np.random.shuffle(val_indices)
+
+    train_datasets = torch.utils.data.Subset(full_train_datasets, train_indices)
+    val_datasets= torch.utils.data.Subset(full_train_datasets, val_indices)
+
+    return train_datasets, val_datasets
 
 def run(name, epochs):
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
@@ -34,13 +57,16 @@ def run(name, epochs):
     )
 
     #split train/val data
-    indices = np.arange(len(full_train_datasets))
-    np.random.seed(42)
-    np.random.shuffle(indices)
-    train_indices = indices[:45000]
-    val_indices = indices[45000:]
+    #train_datasets, val_datasets = split_train_val_data(full_train_datasets)
+    
+    train_indices, val_indices = train_test_split(
+        np.arange(len(full_train_datasets)),
+        test_size = 0.1,
+        stratify = np.array(full_train_datasets.targets),
+        random_state = 42)
+    
     train_datasets = torch.utils.data.Subset(full_train_datasets, train_indices)
-    val_datasets= torch.utils.data.Subset(full_train_datasets, val_indices)
+    val_datasets = torch.utils.data.Subset(full_train_datasets, val_indices)
 
     train_data = torch.utils.data.DataLoader(train_datasets, batch_size = 32, shuffle = True)
     val_data = torch.utils.data.DataLoader(val_datasets, batch_size = 32, shuffle = False)
