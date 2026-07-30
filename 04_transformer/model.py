fed4fa9f0bd2cb4d8a1ccac9ea8e371c2b3f88dc
AJY <skyscraper2936@gmail.com>
2026-07-30 14:53:38 +0900

    feat(04_transformer): implement Transformer encoder-decoder architecture

--- 04_transformer/model.py
+++ 04_transformer/model.py
@@ -35,8 +35,8 @@ class MultiHeadAttention(nn.Module):
         self.wk = nn.Linear(d_model, d_model)
         self.wv = nn.Linear(d_model, d_model)
         self.wo = nn.Linear(d_model, d_model)
-    
-    def forward(self, x):
+
+    def forward(self, x, mask = False):
         batch, seq_len = x.size(0), x.size(1)
 
         q = self.wq(x)
@@ -53,7 +53,14 @@ class MultiHeadAttention(nn.Module):
 
         score = q @ k.transpose(-2, -1)
         score = score / math.sqrt(self.head_dim)
+
+        if mask:
+            causal_mask = torch.triu(torch.ones(seq_len, seq_len, device = x.device), 
+            diagonal = 1)
+            score = score.masked_fill(causal_mask == 1, float('-inf'))
+
         weight = torch.softmax(score, dim = -1)
+
         out = weight @ v
 
         out = out.transpose(1, 2).contiguous()
@@ -150,4 +157,129 @@ class TransformerEncoder(nn.Module):
         for layer in self.layers:
             out = layer(out)
         
+        return out
+
+class CrossAttention(nn.Module):
+    def __init__(self, num_heads, d_model):
+        super().__init__()
+        assert d_model % num_heads == 0 , ('d_model must divided by num_heads')
+
+        self.wq = nn.Linear(d_model, d_model)
+        self.wk = nn.Linear(d_model, d_model)
+        self.wv = nn.Linear(d_model, d_model)
+        self.wo = nn.Linear(d_model, d_model)
+
+        self.num_heads = num_heads
+        self.head_dim = d_model // num_heads
+        
+    def forward(self, x, memory):
+        batch, target_len = x.shape[:2]
+        source_len = memory.size(1)
+
+        q = self.wq(x)
+        k = self.wk(memory)
+        v = self.wv(memory)
+
+        q = q.reshape(batch, target_len, self.num_heads, self.head_dim)
+        k = k.reshape(batch, source_len, self.num_heads, self.head_dim)
+        v = v.reshape(batch, source_len, self.num_heads, self.head_dim)
+
+        q = q.transpose(1, 2)
+        k = k.transpose(1, 2)
+        v = v.transpose(1, 2)
+
+        score = q @ k.transpose(-2, -1)
+        score = score / math.sqrt(self.head_dim)
+        weight = torch.softmax(score, dim = -1)
+        out = weight @ v
+
+        out = out.transpose(1, 2).contiguous()
+
+        out = out.reshape(batch, target_len, self.num_heads * self.head_dim)
+
+        out = self.wo(out)
+
+        return out
+
+class DecoderLayer(nn.Module):
+    def __init__(self, num_heads, d_model, d_ff):
+        super().__init__()
+        self.attn = MultiHeadAttention(num_heads, d_model)
+        self.dropout = nn.Dropout(0.1)
+        self.norm1 = nn.LayerNorm(d_model)
+
+        self.attn2 = CrossAttention(num_heads, d_model)
+        self.norm2 = nn.LayerNorm(d_model)
+
+        self.ffn = nn.Sequential(nn.Linear(d_model, d_ff),
+                                 nn.ReLU(),
+                                 nn.Linear(d_ff, d_model))
+        self.norm3 = nn.LayerNorm(d_model)
+
+    def forward(self, x, memory):
+        out = self.attn(x, mask = True)
+        out = self.dropout(out)
+        out = self.norm1(out + x)
+        
+        out2 = self.attn2(out, memory)
+        out2 = self.dropout(out2)
+        out2 = self.norm2(out2 + out)
+
+        out3 = self.ffn(out2)
+        out3 = self.dropout(out3)
+        out3 = self.norm3(out3 + out2)
+
+        return out3
+
+class TransformerDecoder(nn.Module):
+    def __init__(self, N, vocab_size, d_model, max_len, num_heads, d_ff):
+        super().__init__()
+        self.d_model = d_model
+        self.embedding = nn.Embedding(vocab_size, d_model)
+        self.position = PositionEncoding(max_len, d_model)
+        self.dropout = nn.Dropout(0.1)
+        self.layers = nn.ModuleList([DecoderLayer(num_heads, d_model, d_ff) for _ in range(N)])
+
+    def forward(self, x, memory):
+        x = self.embedding(x) * math.sqrt(self.d_model)
+        x = self.position(x)
+        x = self.dropout(x)
+
+        for layer in self.layers:
+            x = layer(x, memory)
+        
+        return x
+
+class Transformer(nn.Module):
+    def __init__(self,
+                 N,
+                 src_vocab_size,
+                 tgt_vocab_size,
+                 d_model,
+                 src_len,
+                 tgt_len,
+                 num_heads,
+                 d_ff):
+
+        super().__init__()
+        self.encoder = TransformerEncoder(src_vocab_size, 
+                                          d_model, 
+                                          src_len, 
+                                          N, 
+                                          num_heads, 
+                                          d_ff)
+
+        self.decoder = TransformerDecoder(N, 
+                                          tgt_vocab_size,
+                                          d_model,
+                                          tgt_len,
+                                          num_heads,
+                                          d_ff)
+        self.fc = nn.Linear(d_model, tgt_vocab_size)
+    
+    def forward(self, src, tgt):
+        memory = self.encoder(src)
+        out = self.decoder(tgt, memory)
+        out = self.fc(out)
+
         return out
